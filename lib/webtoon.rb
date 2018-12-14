e44c8cccb2153d5363cdbb5442d3e5ef25505260
helloamandamurphy <helloamandamurphy@gmail.com>
2018-12-14 15:50:50 -0500

    Created comics.rb, fixed require dependency in webtoon.rb that was not previously working.

--- lib/webtoon.rb
+++ lib/webtoon.rb
@@ -2,7 +2,7 @@ require "nokogiri"
 require "pry"
 require "open-uri"
 
+require_relative "./webtoon/version"
 require_relative "./webtoon/cli.rb"
 require_relative "./webtoon/comics"
 require_relative "./webtoon/scraper"
-require_relative "./webtoon/version"
