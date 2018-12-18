49b9d32fcfd7b35fa66fce038da1d234689ede59
helloamandamurphy <helloamandamurphy@gmail.com>
2018-12-18 14:11:42 -0500

    Created hash for target_scraper method.

--- lib/webtoon/scraper.rb
+++ lib/webtoon/scraper.rb
@@ -2,7 +2,7 @@
 
 class Webtoon::Scraper
 
-  def self.genre_scraper #
+  def self.genre_scraper #collects url for genre selection
     by_genre = {}
     doc = Nokogiri::HTML(open("https://www.webtoons.com/en/top"))
     genre_links = doc.css("div.left_area ul.ranking_tab").children.css("a").collect {|el| el.attr("href")}
@@ -27,13 +27,29 @@ class Webtoon::Scraper
     by_genre
   end
 
-  def self.target_scraper
+  def self.target_scraper #collects url for target selection
     by_target = {}
     doc = Nokogiri::HTML(open("https://www.webtoons.com/en/top"))
     target_links = doc.css("div.right_area ul.ranking_tab").children.css("a").collect {|el| el.attr("href")}#.each do |link|
-    
     #target_names = doc.css("div.right_area ul.ranking_tab").children.css("a span").collect {|el| el.text.strip}
 
+    target_links.each do |link|
+      link.downcase!
+      if link.include? "male10"
+        by_target[:male10] = link
+      elsif link.include? "female10"
+        by_target[:female10] = link
+      elsif link.include? "male20"
+        by_target[:male20] = link
+      elsif link.include? "female20"
+        by_target[:female20] = link
+      elsif link.include? "male30"
+        by_target[:male30] = link
+      elsif link.include? "female30"
+        by_target[:female30] = link
+      end
+    end
+    by_target
   end
 
   def self.scrape_by_genre #scrapes top 10 comics by genre
@@ -66,8 +82,6 @@ class Webtoon::Scraper
 end
 
 
-#1) Scrape top 10 comics for each genre.
-#2) Scrape top 10 comics for each age/gender.
 #3) Scrape details from individual url.
 
 #Nested Data Structure
@@ -76,7 +90,7 @@ end
     #all = [
       #{rank1}, {rank2,} {rank3}
     #]
-  #by_age_gender = {}
+  #by_target = {}
 #top10comics = [{rank, title, author}, {rank, title, author}, {etc.}]
 
 #comic = {
